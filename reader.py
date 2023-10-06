5bdada5c922f0aeeff511bf39f1fb61e4720f0ba
Josef Hrebec <josefhrebec02@gmail.com>
2023-10-06 17:35:42 +0200

    General revision

--- reader.py
+++ reader.py
@@ -1,9 +1,51 @@
 import feedparser
+import re
 
-url = "https://www.example.com/rss"
-feed = feedparser.parse(url)
+def main():
+    position = 0
+    opened = True
+    titles = []
+    url = "https://cc.cz/feed/"
 
-with open('rss.txt', 'w') as f:
-    f.write(f"{feed}")
+    d = feedparser.parse(url)
+    
+    if d.status != 200:
+        return print("An error has occured.")
 
-print("Done!")
+    try:
+        with open('rss.txt', 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                if re.match("\d", line):
+                    titles.append(re.sub(r'.*: |\n','',line))
+    except OSError:
+        opened = False
+
+    itemField = [i for i in d.entries]
+
+    with open('rss.txt', 'w') as f:
+        for index, i in enumerate(itemField):
+            if index == 0:
+                try:
+                    position = titles.index(i.title) 
+                    # position 0
+                except ValueError:
+                    pass
+            f.write(f"{index+1}: {i.title}\n")
+            f.write(f"{' '*4}- {i.link}\n")
+            desc = re.sub(r'<img.* />|<p>Článek.*/p>|<p>|</p>',r'',i.description)
+            f.write(f"{' '*4}- {desc}\n")
+            f.write("\n")
+        
+    print("\nDone!")
+
+    if opened:
+        if position <= 0:
+            print("No new news found.")
+        else:
+            print(f"Found {position} new news!")
+            
+    print(f"Fetched {len(itemField)} news.\n")
+    
+if __name__ == "__main__":
+    main()
